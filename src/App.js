05ec656b190d57bfc652a4f7b937460d6fe43cc3
Natyoverwatch <natap455@gmail.com>
2023-04-20 23:13:14 -0500

    prueba de cambio

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende con Nathalia React
         </a>
       </header>
     </div>
